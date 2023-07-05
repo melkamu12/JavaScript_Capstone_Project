3d76128e06a492991746a0fc49138edaa65d1b10
MariamOsman11 <mariamkca11@gmail.com>
2023-07-05 18:11:37 +0300

    Modify other files

--- src/module/api.js
+++ src/module/api.js
@@ -1,4 +1,4 @@
-const baseURl = 'www.themealdb.com/api/json/v1/1/filter.php?i=chicken_breast';
+const baseURl = 'https://www.themealdb.com/api/json/v1/1/filter.php?i=chicken_breast';
 
 const APILikeURL = 'https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/fwlndnSGaHXJiW31vz2m/likes/';
 const APICommentURL = 'https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/YeTooaUu7CcnY5iAkHx6/comments/';
